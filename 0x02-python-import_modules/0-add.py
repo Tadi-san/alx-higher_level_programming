acfee993db08805e6d41521963c1a50605e1bb4a
root <tadiosanegagregn@yahoo.com>
2022-06-03 18:29:01 -0700

    s

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
-a=1
-b=2
-from add_0.py import add
-print("<"+str(a)+" value> + <"+str(b)+" value> = <"+str(add(a,b))+" value>")
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
